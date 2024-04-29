d9d5d551b63b4abb7eb240c922a0b4181de6dc89
manugcr <mgilcernich@gmail.com>
2024-04-29 01:32:29 -0300

    Added script to exec gdb and qemu

--- src/build.sh
+++ src/build.sh
@@ -6,7 +6,9 @@ as -g -o main.o protected_mode.S
 echo "  -> Linking..."
 ld --oformat binary -o main.img -T link.ld main.o
 
-echo "  -> Launching QEMU..."
-qemu-system-i386 -drive format=raw,file=main.img,index=0,media=disk
+echo "  -> Launching QEMU in debug mode..."
+gnome-terminal -- qemu-system-i386 -fda main.img -boot a -s -S -monitor stdio
 
-# qemu-system-i386 -fda main.img -boot a -s -S -monitor stdio
+# Open a new terminal window and execute GDB
+echo "  -> Launching GDB linked with QEMU..."
+gnome-terminal -- gdb -x gdb.gdb
